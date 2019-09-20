ccbeecbe63770ae2765f9224aadf14c590782372
daveton <dave.ton@icloud.com>
2019-09-20 19:22:43 +0800

    M 新增 router 文件

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Router from './router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router />, document.getElementById('root'));
 
