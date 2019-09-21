58cf0f6158a3729b67c6ca3324dfefb81b27e3df
daveton_ <dave.zzev@gmail.com>
2019-09-21 15:51:03 +0800

    M home

---
+++ src/pages/home/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Home = () => {
+    return(
+        <div>Hello World</div>
+    )
+}
+
+// const mapToState = (state) => {
+
+// }
+
+// const mapToDispatch = (dispatch) => {
+
+// }
+
+export default connect(null, null)(Home);
