67fec1df414de2b698de785a353fe3d14ac8a3c6
daveton_ <dave.zzev@gmail.com>
2019-09-23 14:06:27 +0800

    M header

--- src/router.js
+++ src/router.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 
+import Header from './common/header';
 import Home from './pages/home';
 
 export default class Router extends React.Component{
     render(){
         return(
             <div className='eu-container'>
+
+                <Header />
+
                 <HashRouter>
                     <Switch>
                         <Route exact path='/' component={Home} />
