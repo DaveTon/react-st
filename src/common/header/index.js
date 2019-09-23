0422b499b8ba6ec2ba9b1a52f3557e6ebbd50154
daveton_ <dave.zzev@gmail.com>
2019-09-23 14:04:37 +0800

    M add header

---
+++ src/common/header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Header = () => {
+    return(
+        <div>Hello Header</div>
+    )
+}
+
+// const mapToState = (state) => {
+
+// }
+
+// const mapToDispatch = (dispatch) => {
+
+// }
+
+export default connect(null, null)(Header);
