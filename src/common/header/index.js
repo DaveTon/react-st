291cd781d499323067f6ce4b89009373b6695b77
daveton <dave.ton@icloud.com>
2019-09-23 21:51:40 +0800

    M eu-header

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import './style.scss';
 
 import Logo from './../../resource/imgs/header/logo.png';
 
 const Header = () => {
     return(
         <header className='eu-header'>
+            <div className='wrap'>
 
-            <Link to='/'>
-                <div className='eu-logo'>
-                    <img src={Logo} alt='nav-logo' />
-                </div>
-            </Link>
+                <Link to='/'>
+                    <div className='eu-logo'>
+                        <img className='img' src={Logo} alt='nav-logo' />
+                    </div>
+                </Link>
 
-            <section className='header-content-wrap'>
-                <div className='content-left'></div>
+                <section className='header-content-wrap'>
+                    <div className='content-left'></div>
+                    
+                    <div className='content-right'></div>
+                </section>
                 
-                <div className='content-right'></div>
-            </section>
-            
+            </div>
         </header>
     )
 }
