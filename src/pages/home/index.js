f4deea88551387241d8a03f9385d5e4dca39f40d
daveton <dave.ton@icloud.com>
2019-09-25 23:44:05 +0800

    M home content

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,7 +6,29 @@ import './style.scss';
 const Home = () => {
     return(
         <article className='container'>
-            Hello Home
+            <div className='note-list'>
+                <ul className='list-wrap'>
+                    <li className='note-item'>
+                        <h2 className='title'>我是标题</h2>
+                        <p className='abstract'>我是段落</p>
+                        <div className='meta'>
+                            <button className='eu-button'>
+                                <i className="icon comments"></i>
+                                <span className='label'>4726.3</span>
+                            </button>
+                            <button className='eu-button'>
+                                <span className='label'>简书</span>
+                            </button>
+                            <button className='eu-button'>
+                                <i className="icon like"></i>
+                                <span className='label'>11676</span>
+                            </button>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            
+            <div className='aside-list'></div>
         </article>
     )
 }
