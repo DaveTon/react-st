6aa2f198abdd64606d631229d8c306d3128ba611
daveton_ <dave.zzev@gmail.com>
2019-09-26 11:15:23 +0800

    M reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
+import { reducer as headerReducer } from '../common/header/store';
 import { reducer as homeReducer } from './../pages/home/store';
+import { reducer as followReducer } from '../pages/follow/store';
 
 const reducer = combineReducers({
-    home: homeReducer
+    header: headerReducer,
+    home: homeReducer,
+    follow: followReducer
 });
 
 export default reducer;
