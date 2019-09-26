cd63d7a986535844f618e90712ac0f0f9950d764
daveton_ <dave.zzev@gmail.com>
2019-09-26 14:26:11 +0800

    M author list

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -9,10 +9,18 @@ import banner1 from '../../resource/imgs/home/banner-s1.png';
 import banner2 from '../../resource/imgs/home/banner-s2.png';
 import banner3 from '../../resource/imgs/home/banner-s3.png';
 import banner4 from '../../resource/imgs/home/banner-s4.png';
+import head1 from '../../resource/imgs/home/head1.jpg';
+import head2 from '../../resource/imgs/home/head2.jpg';
 
 const Home = () => {
     return(
         <article className='container'>
+            <div className='side-tool'>
+                <button className='eu-button'>
+                    <i className='icon dropright'></i>
+                </button>
+            </div>
+
             <div className='note-list'>
                 <ul className='list-wrap'>
                     <li className='note-item'>
@@ -97,19 +105,49 @@ const Home = () => {
             </div>
             
             <div className='aside-list'>
-                <div className='board'>
-                    <a className='img-wrap' target='_bland' href='/'>
-                        <img className='img' src={banner1} alt='banner1' />
-                    </a>
-                    <a className='img-wrap' target='_bland' href='/'>
-                        <img className='img' src={banner2} alt='banner2' />
-                    </a>
-                    <a className='img-wrap' target='_bland' href='/'>
-                        <img className='img' src={banner3} alt='banner3' />
-                    </a>
-                    <a className='img-wrap' target='_bland' href='/'>
-                        <img className='img' src={banner4} alt='banner4' />
-                    </a>
+                <div className='wrap'>
+                    <div className='board'>
+                        <a className='img-wrap' target='_bland' href='/'>
+                            <img className='img' src={banner1} alt='banner1' />
+                        </a>
+                        <a className='img-wrap' target='_bland' href='/'>
+                            <img className='img' src={banner2} alt='banner2' />
+                        </a>
+                        <a className='img-wrap' target='_bland' href='/'>
+                            <img className='img' src={banner3} alt='banner3' />
+                        </a>
+                        <a className='img-wrap' target='_bland' href='/'>
+                            <img className='img' src={banner4} alt='banner4' />
+                        </a>
+                    </div>
+
+                    <div className='authors'>
+                        <div className='header'>
+                            <button className='eu-button title'>
+                                <span className='label'>推荐作者</span>
+                            </button>
+                            <button className='eu-button refresh'>
+                                <i className="icon refresh"></i>
+                                <span className='label'>换一批</span>
+                            </button>
+                        </div>
+
+                        <div className='author-list'>
+                            <div className='item'>
+                                <button className='eu-button img'>
+                                    <img className='img' src={head1} alt='user' />
+                                </button>
+                                <div className='follow'>
+                                    <h3 className='title'>无线猴子</h3>
+                                    <p className='content'>寫了165.9k字 · 3.5k喜歡</p>
+                                </div>
+                                <button className='eu-button'>
+                                    <i className="icon plus"></i>
+                                    <span className='label'>关注</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </article>
