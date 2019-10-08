59869ce2f9799cd2814530823bf7ce8b23f54da2
daveton_ <dave.zzev@gmail.com>
2019-10-08 15:49:56 +0800

    M home reducer

--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -8,7 +8,8 @@ export default (state = defaultState, action) => {
     const newState = JSON.parse(JSON.stringify(state));
     switch(action.type){
         case constants.CHANGE_HOME_DATA:
-            newState.topicList.push(action.data);
+            // newState.topicList.push(action.data);
+            newState.topicList = action.data;
             return newState;
         default:
             return newState;
