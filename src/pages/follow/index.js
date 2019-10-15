fdf736343cd52460b1d3cffc794151d6c00cd405
daveton <dave.ton@icloud.com>
2019-10-15 22:32:38 +0800

    M 调整follow

--- src/pages/follow/index.js
+++ src/pages/follow/index.js
@@ -2,37 +2,39 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './style.scss';
 
-const Follow = () => {
-    return(
-        <article className='container'>
-            <aside className='aside'>
-                <div className='aside-header'>
-                    <div className='header'>
-                        <button className='eu-button'>
-                            <span className='label'>关注全部</span>
-                            <i className="icon dropdown"></i>
-                        </button>
-                        <button className='eu-button add'>
-                            <span className='label'>添加关注</span>
-                            <i className="icon dropdown"></i>
-                        </button>
+class Follow extends React.Component{
+    render(){
+        return(
+            <article className='container'>
+                <aside className='aside'>
+                    <div className='aside-header'>
+                        <div className='header'>
+                            <button className='eu-button'>
+                                <span className='label'>关注全部</span>
+                                <i className="icon dropdown"></i>
+                            </button>
+                            <button className='eu-button add'>
+                                <span className='label'>添加关注</span>
+                                <i className="icon dropdown"></i>
+                            </button>
+                        </div>
+    
+                        <div className='list'>
+                            
+                        </div>
                     </div>
-
-                    <div className='list'>
-                        
-                    </div>
-                </div>
-
-                <ul className='aside-list'>
-                    <li className='item'></li>
-                </ul>
-            </aside>
-
-            <main className='main'>
-
-            </main>
-        </article>
-    )
+    
+                    <ul className='aside-list'>
+                        <li className='item'></li>
+                    </ul>
+                </aside>
+    
+                <main className='main'>
+    
+                </main>
+            </article>
+        )
+    }
 }
 
 // const mapToState = (state) => {
