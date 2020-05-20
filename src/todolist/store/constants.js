287b0ade0bc70980e6398b5acb6f05d0c19e7ff6
Dave Tang <daveton@Dave-MBP.local>
2020-05-20 23:16:06 +0800

    [todolist]

--- src/todolist/store/constants.js
+++ src/todolist/store/constants.js
@@ -1,3 +1,3 @@
-export const CHANGE_INPUT_VALUE = 'CHANGE_INPUT_VALUE';
-export const ADD_VALUE_LIST = 'ADD_VALUE_LIST';
-export const DELETE_ITEM = 'DELETE_ITEM';
+export const CHANGE_INPUT_VALUE = 'todolist/CHANGE_INPUT_VALUE';
+export const ADD_VALUE_LIST = 'todolist/ADD_VALUE_LIST';
+export const DELETE_ITEM = 'todolist/DELETE_ITEM';
