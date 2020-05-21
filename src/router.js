ad540b2df87cc0b9c6bffb98eb7490ea51040f5f
Dave Tang <daveton@Dave-MBP.local>
2020-05-21 20:10:12 +0800

    [jianshu] 测试iconfont

--- src/router.js
+++ src/router.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './resource/scss/essential-ui.scss';
 
 class Router extends React.Component{
     render(){
         return(
-            <div>Hello World</div>
+            <div>
+                <h1>Hello World</h1>
+                <button className='iconfont iconshang'></button>
+            </div>
         )
     }
 }
