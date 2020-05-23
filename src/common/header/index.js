2b931a02bed98f11d827a586001561a9e0e616e9
Dave Tang <daveton@Dave-MBP.local>
2020-05-23 10:48:54 +0800

    [jianshu] header

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import './style.scss';
+
 class Header extends React.Component{
     render(){
         return(
-            <div>Header</div>
+            <header className='eu-header'>
+                <div className='wrap'>
+                    <div className='eu-logo'></div>
+                    <nav className='eu-nav'></nav>
+                    <div className='eu-btnGroup'>
+                        <button className='eu-button'></button>
+                    </div>
+                </div>
+            </header>
         )
     }
 }
