b65f4fc9025dd4a6858434080d039819c778fdd2
Dave <dave.zzev@gmail.com>
2021-10-13 10:48:33 +0800

    init
    #重新编码Todlist功能

---
+++ src/Todolist.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './resource/css/index.css';
+
+class Todolist extends Component{
+    render(){
+        return (
+            <div>Hello World</div>
+        )
+    }
+}
+
+export default Todolist;
