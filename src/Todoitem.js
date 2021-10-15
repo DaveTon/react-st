6316455faae4b18490a553f2d78c5196b4f95618
Dave <dave.zzev@gmail.com>
2021-10-15 10:08:35 +0800

    propTypes & defaultProps

--- src/Todoitem.js
+++ src/Todoitem.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+// import DefaultProps from 'default-props';
 
 class Todoitem extends Component{
     constructor(props){
@@ -6,16 +8,28 @@ class Todoitem extends Component{
         this.deleteItem=this.deleteItem.bind(this);
     }
     render(){
+        const { item, test } = this.props
         return(
             <li
                 onClick={this.deleteItem}
-            >{this.props.item}</li>
+            >{test}-{item}</li>
         )
     }
     deleteItem(){
         const { handelDeleteItem, index } = this.props;
         handelDeleteItem(index)
     }
+}
+
+Todoitem.propTypes = {
+    tset: PropTypes.string.isRequired,
+    item: PropTypes.string,
+    index: PropTypes.number,
+    handelDeleteItem: PropTypes.func
+}
+
+Todoitem.defaultProps = {
+    test: 'Hello World'
 }
 
 export default Todoitem;
