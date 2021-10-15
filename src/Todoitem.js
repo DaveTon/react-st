c26c0de75bb0e0ddb0455d7af3349fb1c122a713
Dave <dave.zzev@gmail.com>
2021-10-15 15:02:23 +0800

    删除sass文件

--- src/Todoitem.js
+++ src/Todoitem.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import DefaultProps from 'default-props';
 
 class Todoitem extends Component{
     constructor(props){
